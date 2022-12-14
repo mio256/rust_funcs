159e4fe7c4d8dedcd1aba58ad4928e6df048bf40
mio256 <reiya@woorth.io>
2022-12-14 14:48:50 +0900

    :fire: add Associated Functions

--- src/impl.rs
+++ src/impl.rs
@@ -8,18 +8,25 @@ impl Rectangle {
     fn area(&self) -> usize {
         self.width * self.height
     }
+
+    fn can_hold(&self, rect:&Rectangle) -> bool {
+        self.width > rect.width && self.height > rect.height
+    }
+
+    fn square(size:usize) -> Rectangle{
+        Rectangle { width: size, height: size }
+    }
 }
 
 fn main() {
-    let rectangle1 = Rectangle {
-        width: 40,
-        height: 30,
-    };
+    let rect1 = Rectangle { width: 30, height: 50 };
+    let rect2 = Rectangle { width: 10, height: 40 };
+    let rect3 = Rectangle { width: 60, height: 45 };
+    let rect4 = Rectangle::square(50);
 
-    println!(
-        "The area of the rectangle is {} square pixels.",
-        rectangle1.area()
-    );
+    // rect1にrect2ははまり込む？
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 
-    dbg!(rectangle1);
+    dbg!(&rect4);
 }
